51e3d17e1873a91389306f5f502ee8c7068e474b
2541773-wits <2541773@students.wits.ac.za>
2024-04-10 20:31:57 +0200

    change endpoints

--- src/script.js
+++ src/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 function addCar(newCar) {
-    fetch('/api/message/cars', {
+    fetch('/api/message', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -60,7 +60,7 @@ carForm.addEventListener('submit', event => {
 // Function to remove a car
 function removeCar(index) {
     const carId = cars[index].id;
-    fetch(`/api/message/cars/${carId}`, {
+    fetch(`/api/message/${carId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
